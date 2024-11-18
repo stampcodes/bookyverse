051cfcf85157084d261db71301b71f96a41fc697
Andre <andreafragnelli@gmail.com>
2024-11-18 10:45:28 +0100

    feat(router): add routing for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import DescriptionPage from "./pages/DescriptionPage";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/description" element={<DescriptionPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
